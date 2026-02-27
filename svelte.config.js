0530bca5d931552ddefe73eb93260221c4430861
Bruno Afonso <bruno@wearesingular.com>
2026-02-27 14:17:13 +0000

    fixing build issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
-  }
+    adapter: adapter(),
+  },
 };
 
 export default config;
